da0609cd183dec1950d15df2970b386db92e0f08
Yevhen Batsula <jbat98@gmail.com>
2025-03-31 13:52:21 +0300

    lesson4 homework: tasks 1-4 added in new jsBasis folder

---
+++ jsBasics/task4.3.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+let radius = 5.3;
+let height = 10.2;
+let volume = (Math.PI * Math.pow(radius, 2) * height).toFixed(2);
+console.log("Radius of the cylinder is: " + chalk.green(radius));
+console.log("Height of the cylinder is: " + chalk.green(height));
+console.log("Volume of the cylinder with " + radius + " and " + height + " is " + chalk.green(volume));
