b167323e362269db8bad050518f9bc34bd8968e3
Yevhen Batsula <jbat98@gmail.com>
2025-04-21 14:13:26 +0300

    Lesson 9 hometasks added

---
+++ jsBasics/lesson9/task3.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+
+const car1 = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+}
+
+const car2 = {
+    brand: "Honda",
+    model: "Civic",
+    year: 2019,
+}
+
+const car3 = {...car1, ...car2};
---
+++ jsBasics/lesson9/task5.js
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+
+const users = [
+    {
+        name: "John",
+        age: 30,
+        email: "test@test.com",
+    },
+    {
+        name: "Jane",
+        age: 25,
+        email: "",
+    },
+    {
+        name: "Bob",
+        age: 35,
+        email: "bobTester@test.com",
+    },]
+
+    for (const { name, age, email } of users){
+        console.log(chalk.blue(`Name: ${name}`), chalk.yellow(`Age: ${age}`), chalk.red(`Email: ${email}`));
+    }
