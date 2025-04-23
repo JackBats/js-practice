367dad0d35830276aeb6faa87529519270a24324
Yevhen Batsula <jbat98@gmail.com>
2025-04-23 16:25:45 +0300

    - Book.js and EBook.js classes edded with methods and setters and getters; - books.js and ebooks.js files - instances of Book.js and EBooks.js classes are declared, and where class methods are called and checked - booksCataloge.js: 1. add output for Book and EBook instances with printInfo output 2. implement findOldestBook method and display result 3. create EBook from Book using static method

---
+++ jsBasics/lesson10/EBook.js
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import { Book } from "./Book.js";
+
+export class EBook extends Book {
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.fileFormat = fileFormat;
+  }
+    printInfo() {
+        console.log(`${this.title} by ${this.author}, published in ${this.year} year, File format: ${this.fileFormat}`);
+    }
+
+    set fileFormat(format) {
+        if (typeof format !== "string" || format.length === 0) {
+            console.log(chalk.red("Invalid file format"));
+        } else {
+            this._fileFormat = format;
+        }
+    }
+    get fileFormat() {
+        return this._fileFormat;
+    }
+    
+    static bookFrom(book, fileFormat) {
+        if (!(book instanceof Book)) {
+            throw new Error("Provided argument is not a Book instance");
+        }
+            return new EBook(book.title, book.author, book.year, fileFormat);
+    }
+}
+
---
+++ jsBasics/lesson10/ebooks.js
@@ -0,0 +1,29 @@
+import { Book } from "./Book.js";
+import { EBook } from "./EBook.js";
+
+
+const ebook1 = new EBook("The Great Gatsby", "F. Scott Fitzgerald", 1925, "PDF");
+const ebook2 = new EBook("To Kill a Mockingbird", "Harper Lee", 1960, "EPUB");
+const ebook3 = new EBook("1984", "George Orwell", 1949, "MOBI");
+
+
+console.log(ebook1);
+ebook1.title = "Fake Book Title";
+ebook1.author = "Fake Author";
+ebook1.year = 2025;
+ebook1.fileFormat = "ABC";
+ebook1.printInfo();
+
+console.log(ebook2);
+ebook2.title = "";
+ebook2.author = "";
+ebook2.year = -45;
+ebook2.fileFormat = 546;
+ebook2.printInfo();
+
+console.log(ebook3);
+ebook3.title = 435
+ebook3.author = 4366;
+ebook3.year = 2034;
+ebook3.fileFormat = 0;
+ebook3.printInfo();
