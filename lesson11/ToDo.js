7981f477a98e879efa49257f6eb397ee7fc180f8
Yevhen Batsula <jbat98@gmail.com>
2025-04-30 17:10:40 +0300

    Lesson11 hometasks added

---
+++ lesson11/ToDo.js
@@ -0,0 +1,16 @@
+export class ToDo {
+    getTodo(toDoId) {
+        return fetch(`https://jsonplaceholder.typicode.com/todos/${toDoId}`)
+    }
+
+    async printToDo(toDoId) {
+        try {
+            const toDoData = await this.getTodo(toDoId)
+            const body = await toDoData.json()
+            console.log('To do info: ', body)
+        }
+        catch (error) {
+            console.error('Error:', error)
+        }
+    }
+}
---
+++ lesson11/task1.js
@@ -0,0 +1,8 @@
+function textInTime(text, time){
+    setTimeout(() => {
+        console.log(text);
+    }
+    , time);
+}
+
+textInTime('Hello World!', 8000);
